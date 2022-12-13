6215d67a41051720240be11737def3fbbfac5d52
Paul Kim <pkim@hey.com>
2022-12-12 21:11:38 -0500

    Set shadows

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function SpinningBox({ args, color, position }) {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh castShadow position={position} ref={mesh}>
       <boxBufferGeometry attach="geometry" args={args} />
       <meshStandardMaterial attach="material" color={color} />
     </mesh>
@@ -16,24 +16,33 @@ function SpinningBox({ args, color, position }) {
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{ position: [-5,0,10], fov: 50 }}>
-        <ambientLight intensity={1} />
-        <directionalLight position={[10, 1, 1]} intensity={1} shadow-mapSize-width={1024} shadow-mapSize-height={1024} shadow-camera-far={50} shadow-camera-left={-10} shadow-camera-right={10} shadow-camera-top={10} shadow-camera-bottom={-10} />
-        <pointLight position={[-10, 0, -20]} intensity={1} />
-        <pointLight position={[0, -10, 0]} intensity={1} />
-
-
-
+      <Canvas
+        shadows
+        colormanagement="true"
+        camera={{ position: [-5,0,10], fov: 50 }}
+      >
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
 
         <group>
-          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -5, 0]}>
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -5, 0]}>
             <planeBufferGeometry attach="geometry" args={[100,100]} />
-            <meshStandardMaterial attach="material" color="gray" />
+            <shadowMaterial attach="material" opacity={0.3}/>
           </mesh>
         </group>
-
-
-
 
         <SpinningBox args={[3, 5, 2]} position={[-2, 1, -5]} color="#ef4649" />
         <SpinningBox args={[5, 2, 3]} position={[6, -2, -15]} color="#f5c364" />
