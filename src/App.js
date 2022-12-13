53fa350330332d4c46e3fec5841e11768cfaacd9
Paul Kim <pkim@hey.com>
2022-12-12 19:55:58 -0500

    Reset basic intro

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ function App() {
   return (
     <div>
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
       </Canvas>
     </div>
   );
