2c39335a7ba48f7db42a58d1d84ef54d59848bd3
Paul Kim <pkim@hey.com>
2022-12-12 20:22:20 -0500

    Add position props to vary position for boxes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 
-function SpinningBox() {
+function SpinningBox({ position }) {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (
-    <mesh ref={mesh}>
+    <mesh position={position} ref={mesh}>
       <boxBufferGeometry attach="geometry" args={[2,2,2]} />
       <meshStandardMaterial attach="material" color="#ef4649" />
     </mesh>
@@ -18,9 +18,9 @@ function App() {
     <>
       <Canvas colorManagement camera={{ position: [-5,2,10], fov: 60 }}>
         <ambientLight intensity={1} />
-        <SpinningBox />
-        <SpinningBox />
-        <SpinningBox />
+        <SpinningBox position={[0, 1, 0]} />
+        <SpinningBox position={[-2, 1, -5]} />
+        <SpinningBox position={[5, 1, 1]} />
       </Canvas>
     </>
   );
