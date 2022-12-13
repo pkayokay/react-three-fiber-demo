69995a06165886932b1c6cd79f345d44595f3773
Paul Kim <pkim@hey.com>
2022-12-12 20:55:03 -0500

    Fix canvas

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ function SpinningBox({ args, color, position }) {
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{ position: [-5,2,10], fov: 60 }}>
+      <Canvas colorManagement camera={{ position: [-5,0,10], fov: 50 }}>
         <ambientLight intensity={1} />
         <directionalLight position={[10, 1, 1]} intensity={1} shadow-mapSize-width={1024} shadow-mapSize-height={1024} shadow-camera-far={50} shadow-camera-left={-10} shadow-camera-right={10} shadow-camera-top={10} shadow-camera-bottom={-10} />
         <pointLight position={[-10, 0, -20]} intensity={1} />
         <pointLight position={[0, -10, 0]} intensity={1} />
         <SpinningBox args={[3, 5, 2]} position={[-2, 1, -5]} color="#ef4649" />
         <SpinningBox args={[5, 2, 3]} position={[6, -2, -15]} color="#f5c364" />
-        <SpinningBox args={[2, 2, 1]} position={[0, 4, 0]} color="#0076cb" />
-        <SpinningBox args={[1, 2, 3]} position={[5, 4, 1]} color="#00b485" />
+        <SpinningBox args={[2, 2, 1]} position={[0, 3, 0]} color="#0076cb" />
+        <SpinningBox args={[1, 2, 3]} position={[5, 3, 1]} color="#00b485" />
         <SpinningBox args={[2, 2, 4]} position={[4, 1, -2]} color="#2e3042" />
-        <SpinningBox args={[3, 2, 2]} position={[6, 1, 3]} color="#bdbdbd" />
+        <SpinningBox args={[3, 2, 2]} position={[6, 0, 3]} color="#bdbdbd" />
       </Canvas>
     </>
   );
