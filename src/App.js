44908262474d7a2b50134b1058f7eb072b8035d0
Paul Kim <pkim@hey.com>
2022-12-12 20:47:56 -0500

    Clean up position to see all clearly

--- src/App.js
+++ src/App.js
@@ -22,11 +22,11 @@ function App() {
         <pointLight position={[-10, 0, -20]} intensity={1} />
         <pointLight position={[0, -10, 0]} intensity={1} />
         <SpinningBox args={[3, 5, 2]} position={[-2, 1, -5]} color="#ef4649" />
-        <SpinningBox args={[2, 2, 1]} position={[0, 1, 0]} color="#0076cb" />
-        <SpinningBox args={[1, 2, 3]} position={[5, 1, 1]} color="#00b485" />
-        <SpinningBox args={[3, 2, 2]} position={[7, 1, 3]} color="#bdbdbd" />
+        <SpinningBox args={[5, 2, 3]} position={[6, -2, -15]} color="#f5c364" />
+        <SpinningBox args={[2, 2, 1]} position={[0, 4, 0]} color="#0076cb" />
+        <SpinningBox args={[1, 2, 3]} position={[5, 4, 1]} color="#00b485" />
         <SpinningBox args={[2, 2, 4]} position={[4, 1, -2]} color="#2e3042" />
-        <SpinningBox args={[5, 2, 1]} position={[2, 1, -15]} color="#f5c364" />
+        <SpinningBox args={[3, 2, 2]} position={[6, 1, 3]} color="#bdbdbd" />
       </Canvas>
     </>
   );
